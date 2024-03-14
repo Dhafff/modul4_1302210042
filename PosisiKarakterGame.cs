c94d462181f5faf6c869c44fb517472235740468
Dhafff <dhafgoes@student.telkomuniversity.ac.id>
2024-03-14 11:12:17 +0700

    menambahkan class PosisiKarakterGame'

--- PosisiKarakterGame.cs
+++ PosisiKarakterGame.cs
@@ -8,5 +8,22 @@ namespace modul4_1302210042
 {
     internal class PosisiKarakterGame
     {
+        public interface State
+        {
+            void Terbang();
+            void Jongkok();
+        }
+
+        public class Position : State
+        {
+            public void Terbang()
+            {
+                Console.WriteLine("Posisi landing");
+            }
+            public void Jongkok()
+            {
+                Console.WriteLine("Posisi take off");
+            }
+        }
     }
 }
